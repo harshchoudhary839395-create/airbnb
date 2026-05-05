1b7a3806d874fadf7518df1a98e71c7cfc21be00
Harsh Choudhary <harshchoudhary839395@gmail.com>
2026-05-05 23:30:20 +0530

    add models according to MVC architucture

--- src/controllers/homes.js
+++ src/controllers/homes.js
@@ -1,22 +1,25 @@
+const Home = require("../models/home");
 
-
- exports.getAddHome = (req, res, next) => {
+exports.getAddHome = (req, res, next) => {
   res.render('addHome', {pageTitle: 'Add Home to airbnb'});
 };
 
-const registeredHomes = [];
-
 exports.postAddHome = (req, res, next) => {
   console.log('Home Registration successful for:', req.body);
-  registeredHomes.push(req.body);
+  const {houseName, price, location, rating, photoUrl} = req.body;
+  const home = new Home(houseName, price, location, rating, photoUrl);
+  home.save();
+
+
   res.render('homeAdded', {pageTitle: 'Home Added Successfully'});
   console.log(registeredHomes)
 };
 
 exports.getAddHomes = (req, res, next) => {
+  const registeredHomes = Home.fetchAll();
   // console.log(registeredHomes);
   res.render('home', {registeredHomes: registeredHomes, pageTitle: 'airbnb Home'});
 };
 
 
-exports.registeredHomes = registeredHomes;
+//exports.registeredHomes = registeredHomes;
